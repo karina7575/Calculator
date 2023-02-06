5f5fcf3a81d74a22bec596fa0883abca4a8fa97b
karina7575 <karinakios@mail.ru>
2023-02-06 13:58:42 +0300

    06.02.23

---
+++ Exceptions.java
@@ -0,0 +1,6 @@
+public class Exceptions extends Exception
+{
+    public Exceptions(String description) {
+        super (description);
+    }
+}
--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
+import java.io.IOException;
+
 public class Main
 {
-    public static void main (String[] args)
-    {
-        int a,b;
-        String str1 = "X*X";
+    public static void main (String[] args) throws Exceptions {
+        int a,b, result;
+        String str1 = "III-VI";
+
         try
         {
             if (str1.indexOf(43) != -1)
@@ -12,7 +14,8 @@ public class Main
                 if (((strings[0].indexOf(73) != -1) || (strings[0].indexOf(86) != -1) || (strings[0].indexOf(88) != -1)) && ((strings[1].indexOf(73) != -1) || (strings[1].indexOf(86) != -1) || (strings[1].indexOf(88) != -1))) {
                     a = RomToArabic(strings[0]);
                     b = RomToArabic(strings[1]);
-                    System.out.println(a + b);
+                    result = a+b;
+                    ArabicToRom(result);
                 } else {
                     a = Integer.parseInt(strings[0]);
                     b = Integer.parseInt(strings[1]);
@@ -25,8 +28,13 @@ public class Main
                 if (((strings[0].indexOf(73) != -1) || (strings[0].indexOf(86) != -1) || (strings[0].indexOf(88) != -1)) && ((strings[1].indexOf(73) != -1) || (strings[1].indexOf(86) != -1) || (strings[1].indexOf(88) != -1))) {
                     a = RomToArabic(strings[0]);
                     b = RomToArabic(strings[1]);
-                    System.out.println(a - b);
-                } else {
+
+                    result = a - b;
+                    if (result < 0) throw new Exceptions("в римской системе нет отрицательных чисел");
+                    ArabicToRom(result);
+                }
+
+                    else {
                     a = Integer.parseInt(strings[0]);
                     b = Integer.parseInt(strings[1]);
                     System.out.println(a - b);
@@ -38,7 +46,8 @@ public class Main
                 if (((strings[0].indexOf(73) != -1) || (strings[0].indexOf(86) != -1) || (strings[0].indexOf(88) != -1)) && ((strings[1].indexOf(73) != -1) || (strings[1].indexOf(86) != -1) || (strings[1].indexOf(88) != -1))) {
                     a = RomToArabic(strings[0]);
                     b = RomToArabic(strings[1]);
-                    System.out.println(a * b);
+                    result = a*b;
+                    ArabicToRom(result);
                 }
                 else
                 {
@@ -53,14 +62,16 @@ public class Main
                 if (((strings[0].indexOf(73) != -1) || (strings[0].indexOf(86) != -1) || (strings[0].indexOf(88) != -1)) && ((strings[1].indexOf(73) != -1) || (strings[1].indexOf(86) != -1) || (strings[1].indexOf(88) != -1))) {
                     a = RomToArabic(strings[0]);
                     b = RomToArabic(strings[1]);
-                    System.out.println(a / b);
+                    result = a/b;
+                    ArabicToRom(result);
                 } else {
                     a = Integer.parseInt(strings[0]);
                     b = Integer.parseInt(strings[1]);
                     System.out.println(a / b);
                 }
             }
-        } catch (NumberFormatException e)
+            else throw new IOException();
+        } catch (NumberFormatException | IOException e)
         {
             System.out.println("используются одновременно разные системы счисления");
         }
@@ -79,8 +90,48 @@ public class Main
         else if (value.equals("X")) return 10;
         return 0;
     }
-    static <string> string ArabicToRom(int value)
+    static void ArabicToRom(int result)
     {
-        
+        int des = result / 10, ed = result % 10;
+        if (des <= 3)
+        {
+            for(int i = 0; i < des; i++)
+                System.out.print("X");
+        }
+        else if (des == 4) System.out.print("XL");
+        else if (des <= 8)
+        {
+            System.out.print("L");
+            for(int i = 5; i < des; i++)
+                System.out.print("X");
+        }
+        else if (des >= 9)
+        {
+            for(int i = 10; i > des; i--)
+            {
+                System.out.print("X");
+            }
+            System.out.print("C");
+        }
+        if (ed <= 3)
+        {
+            for(int i = 0; i < ed; i++)
+                System.out.print("I");
+        }
+        else if (ed == 4) System.out.print("IV");
+        else if (ed <= 8)
+        {
+            System.out.print("V");
+            for(int i = 5; i < ed; i++)
+                System.out.print("I");
+        }
+        else if (ed >= 9)
+        {
+            for(int i = 10; i > ed; i--)
+            {
+                System.out.print("I");
+            }
+            System.out.print("X");
+        }
     }
 }
